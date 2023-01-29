25e17e611af406dcaf7aea0e591d061aa7631e68
Alejandro Ortiz <ajortiz@princeton.edu>
2023-01-29 00:51:09 -0500

    update config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+let assetPrefix = ''
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+}
 const nextConfig = {
+  assetPrefix: assetPrefix,
   reactStrictMode: true,
   images: {
     unoptimized: true
